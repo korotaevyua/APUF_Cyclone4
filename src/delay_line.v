400420ac09cd661af73bda4373b2a480138d2266
korotaevyua <korotaev240297@gmail.com>
2024-09-08 02:51:47 +0300

    fix errors, add quartus files

--- src/delay_line.v
+++ src/delay_line.v
@@ -1,43 +1,45 @@
-MIT License
+//MIT License
+//
+//Copyright (c) 2024 Yuri Korotaev
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+//
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
 
-Copyright (c) 2024 Yuri Korotaev
+module delay_line
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+# (parameter line_length = 3)
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-
-parameter line_length = 3;
-
-module delay_line(
+(
 	input clk,
 	input [line_length - 1 :0] challenge,
 	output line_out_1,
 	output line_out_2
 );
 
-wire [2 * line_length + 1 : 0] net;
+(* keep *) wire [2 * line_length + 1 : 0] net;
 
 assign net[0] = clk;
 assign net[1] = clk;
 
+genvar i;
 generate
-	genvar i;
-	for (i=1; i<= line_length; i = i +1)
-	begin
+	for (i=1; i<= line_length; i = i +1) 
+	begin : mux_line
 		mux inst_mux_1(
 			.in1(net[i * 2 - 2]),
 			.in2(net[i * 2 - 1]),
@@ -51,7 +53,6 @@ generate
 			.select(challenge[i-1]),
 			.out_mux(net[i * 2 + 1])
 		);
-
 	end
 endgenerate
 
